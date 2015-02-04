4bda0868cb5abf86763175cc24d1f9bf13fb9af4
Hendrik Mans <hendrik@mans.de>
2015-02-04 22:55:34 +0100

    Render a post type’s own form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def clean_html(source)
     Formatter.new(source).sanitize.to_s.html_safe
   end
+
+  def render_post_form(form)
+    render "#{form.object.type.gsub('.', '/').tableize}/form", form: form, post: form.object
+  rescue ActionView::MissingTemplate
+    content_tag(:p) { "No form found for this post type." }
+  end
 end
