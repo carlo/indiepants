3de8e118311e477b21350965777ea1775c0f2e0b
Hendrik Mans <hendrik@mans.de>
2015-02-07 20:35:58 +0100

    Fetch document through HTTParty

--- app/controllers/webmentions_controller.rb
+++ app/controllers/webmentions_controller.rb
@@ -5,8 +5,24 @@ class WebmentionsController < ApplicationController
     @source = params.require(:source)
     @target = params.require(:target)
 
-    # TODO: lots of stuff.
-
     render text: "OK", status: 202
+
+    Thread.new do
+      process_webmention(@source, @target)
+    end
+  end
+
+private
+
+  def process_webmention(source, target)
+    # check if target is on this domain
+    return false unless URI(target).host == current_site.host
+
+    # fetch source document
+    doc = HTTParty.get(source)
+
+    # test if source document actually links to target
+    # extract microformat data from source document
+    # store webmention
   end
 end
