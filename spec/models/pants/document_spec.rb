1949995761088fba78cd68a66dedda400b7419f3
Hendrik Mans <hendrik@mans.de>
2015-02-16 21:35:03 +0100

    Spec the link population

--- spec/models/pants/document_spec.rb
+++ spec/models/pants/document_spec.rb
@@ -43,4 +43,21 @@ describe Pants::Document do
       expect(Pants::Document.at_url(subject.url)).to eq(subject)
     end
   end
+
+  context "when saving" do
+    subject { build :document }
+
+    it "automatically populates its outgoing links" do
+      stub_request(:get, "http://www.planetcrap.com/").
+         to_return(status: 200, body: "<html><head><title>PlanetCrap!</title></head><body></body></html>", headers: {})
+
+      subject.html = %[Here's a <a href="http://www.planetcrap.com/" rel="foo">link</a>!]
+
+      expect { subject.save! }.to change { Pants::Link.count }.by(1)
+      link = Pants::Link.newest.take
+      expect(link.source).to eq(subject)
+      expect(link.target.url).to eq("http://www.planetcrap.com/")
+      expect(link.rel).to eq("foo")
+    end
+  end
 end
