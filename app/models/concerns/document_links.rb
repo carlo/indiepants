5e680243c30109978d7ef310bad44e34048b4105
Hendrik Mans <hendrik@mans.de>
2015-02-16 23:55:55 +0100

    Simplify link population

--- app/models/concerns/document_links.rb
+++ app/models/concerns/document_links.rb
@@ -27,16 +27,16 @@ concern :DocumentLinks do
       link.source = self
       link.rel    = a['rel']   # TODO: or analyze CSS
 
-      target = Pants::Document.from_url(a['href'], fetch: false)
+      target = Pants::Document.at_url(a['href'])
 
       # We're only interested in links to existing local documents.
-      if target.local? && target.persisted?
+      if target.try { local? && persisted? }
         link.target = target
         link.save!
 
         # We don't need to delete this one
         marked_for_deletion.delete(link.id)
-      elsif target.remote?
+      elsif target.try { remote? }
         # TODO: send webmention
         # ...and discard this link
       end
